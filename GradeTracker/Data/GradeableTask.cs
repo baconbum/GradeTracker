d1b8a2b243e9eef4bc5196a7082766e6788e3172
baconbum <benaldebert@gmail.com>
2017-12-04 20:27:26 -0500

    Added GradeableTask class.
    This will contain a gradeable task (e.g. assignment, test) assigned to a course.

---
+++ GradeTracker/Data/GradeableTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GradeTracker.Data
+{
+	public class GradeableTask
+	{
+		#region Class properties
+		public int Id {
+			get;
+			set;
+		}
+
+		public int CourseId {
+			get;
+			set;
+		}
+
+		public string Name {
+			get;
+			set;
+		}
+
+		public DateTime DueDate {
+			get;
+			set;
+		}
+
+		public double PotentialMarks {
+			get;
+			set;
+		}
+
+		public double Weight {
+			get;
+			set;
+		}
+		#endregion
+
+		public GradeableTask(int id, int courseId, string name, DateTime dueDate, double potentialMarks, double weight)
+		{
+			Id =				id;
+			CourseId =			courseId;
+			Name =				name;
+			DueDate =			dueDate;
+			PotentialMarks =	potentialMarks;
+			Weight =			weight;
+		}
+	}
+}
+
